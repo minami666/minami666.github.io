e6d1c1c9ea241489c5169fb1a2e9e4c05167fc49
中里南 <minami.rage@gmail.com>
2020-07-20 09:22:53 +0900

    slick設定

--- 002_etrend/js/common.js
+++ 002_etrend/js/common.js
@@ -2,6 +2,16 @@ $(function(){
 	$('.show__box__right__middle__top--3').click(function(){
 		$(this).css('color', '#000');
   });
-  $('.slider1__box').slick({
+
+  $(".bottom_fix__bottom__btn").click(function(){
+    $("body,html").animate({ scrollTop: 0 }, 500)
+  });
+
+  $('.slider__box').slick({
+    slidesToShow:6,
+    slidesToScroll:1,
+    arrows: true,
+    autoplay: true,
+    dots: true
   })
 });
